021b018bb7a19da9d9079a59b5de87b22b3742a0
jieyingda <850993175@qq.com>
2023-03-10 14:23:24 +0800

    1)继续训练参数改为False

--- yolov5-7.0/train.py
+++ yolov5-7.0/train.py
@@ -440,7 +440,7 @@ def parse_opt(known=False):
     parser.add_argument('--batch-size', type=int, default=12, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=480, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=True, help='resume most recent training')
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
     parser.add_argument('--noval', action='store_true', help='only validate final epoch')
     parser.add_argument('--noautoanchor', action='store_true', help='disable AutoAnchor')
